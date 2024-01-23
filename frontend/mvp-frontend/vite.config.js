dbe64a18f306a4094a60ce25737379fcd26b88ef
Hazar Karadag <parhasard@outlook.de>
2024-01-23 18:05:26 +0100

    change port to 8000

--- frontend/mvp-frontend/vite.config.js
+++ frontend/mvp-frontend/vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {    // <-- this object is added
+    port: 8000
+  },
   plugins: [vue()],
 })
