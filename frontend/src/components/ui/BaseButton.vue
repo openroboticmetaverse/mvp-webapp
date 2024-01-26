16be3aff6222f96eb1129788a6823537b8d0bf15
Hazar Karadag <parhasard@outlook.de>
2024-01-27 00:35:07 +0100

    add layout, add ui components

---
+++ frontend/src/components/ui/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="button m-2 px-4 py-2 bg-transparent border-0 border-white rounded text-white font-bold uppercase text-sm tracking-wide cursor-pointer transition-all duration-300 ease-in-out outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+    :class="{
+      'hover:bg-blue-500 hover:scale-110 hover:text-blue-300 hover:border-blue-300':
+        !disabled,
+    }"
+    :disabled="disabled"
+    @click="onClick"
+  >
+
+      <slot class="w-6 h-6 "></slot>
+
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  disabled: Boolean,
+});
+
+const emit = defineEmits(["click"]);
+
+const onClick = (event: Event) => {
+  if (!props.disabled) {
+    emit("click", event);
+  }
+};
+</script>
