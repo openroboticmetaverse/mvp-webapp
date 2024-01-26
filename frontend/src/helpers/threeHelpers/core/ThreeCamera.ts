5f16c14ab28639a98a31bcf7d28f8de78405720a
Hazar Karadag <parhasard@outlook.de>
2024-01-27 00:58:17 +0100

    increase light

--- frontend/src/helpers/threeHelpers/core/ThreeCamera.ts
+++ frontend/src/helpers/threeHelpers/core/ThreeCamera.ts
@@ -6,6 +6,6 @@ export class ThreeCamera extends THREE.PerspectiveCamera {
   constructor(aspectRatio: number) {
     super(FOV, aspectRatio, NEAR, FAR)
 
-    this.position.set(15, 15, 15)
+    this.position.set(15, 12, 12)
   }
 }
