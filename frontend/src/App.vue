3f317ef7d30e7ed52b16a7619ac522fa5b89c0c2
Hazar Karadag <parhasard@outlook.de>
2024-01-28 18:56:56 +0100

    remove unused styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,13 +15,13 @@ import DefaultLayout from "./layouts/DefaultLayout.vue"
   box-sizing: border-box;
 }
 
-html {
+/* html {
   --font-family-sans-serif: -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif,
     "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
   --font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas,
     "Liberation Mono", "Courier New", monospace;
-}
+} */
 body {
   margin: 0;
   overflow: hidden;
