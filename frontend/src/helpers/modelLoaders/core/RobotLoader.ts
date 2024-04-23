b84f1af6cff0787b1068264bbb299eb4a2ad0ea9
Karim Siala <karimsiala97@gmail.com>
2024-04-23 18:40:29 +0200

    add docstring comments to robotloader

--- frontend/src/helpers/modelLoaders/core/RobotLoader.ts
+++ frontend/src/helpers/modelLoaders/core/RobotLoader.ts
@@ -2,17 +2,39 @@ import { LoaderUtils, Object3D } from "three";
 import URDFLoader from "urdf-loader";
 import { XacroLoader } from "xacro-parser";
 
-const FRANKA_DESCRIPTION_URL = "https://raw.githubusercontent.com/openroboticmetaverse/mvp-test/master/assets/models/franka_description/";
-
+/**
+ * The RobotLoader class is used to create a robot object from a specified URL.
+ */
 export default class RobotLoader {
+  /**
+   * Creates a robot object from the specified URL.
+   *
+   * @param {string} _url - The URL of the robot.
+   * @return {Promise<Object3D>} A promise that resolves to the created robot object.
+   */
   static createRobot(_url: string): Promise<Object3D> {
+    /**
+     * Load robot from URL.
+     *
+     * @param {string} _url - The URL of the robot.
+     * @return {Promise<Object3D>} A promise that resolves to the created robot object.
+     */
     console.log("Loading robot from URL:", _url);
 
+    /**
+     * XacroLoader instance used to load XACRO files.
+     */
     const xacroLoader = new XacroLoader();
     xacroLoader.inOrder = true;
     xacroLoader.requirePrefix = true;
     xacroLoader.localProperties = true;
 
+    /**
+     * Function used to find the URL of a specific package.
+     *
+     * @param {string} pkg - The name of the package.
+     * @return {string} The URL of the package.
+     */
     xacroLoader.rospackCommands = {
       find(pkg) {
         return pkg === "franka_description" ? FRANKA_DESCRIPTION_URL : pkg;
@@ -20,14 +42,30 @@ export default class RobotLoader {
     };
 
     return new Promise((resolve, reject) => {
+      /**
+       * Load XACRO file.
+       *
+       * @param {string} xml - The XML content of the XACRO file.
+       */
       xacroLoader.load(
         _url,
         (xml) => {
+          /**
+           * URDFLoader instance used to parse URDF files.
+           */
           const urdfLoader = new URDFLoader();
           urdfLoader.workingPath = LoaderUtils.extractUrlBase(_url);
+          /**
+           * Parse URDF XML and return the created robot object.
+           */
           const robot = urdfLoader.parse(xml);
           resolve(robot);
         },
+        /**
+         * Handle loading error.
+         *
+         * @param {Error} error - The error that occurred during loading.
+         */
         (error) => {
           console.error("Error loading robot:", error);
           reject(error);
@@ -36,3 +74,4 @@ export default class RobotLoader {
     });
   }
 }
+
