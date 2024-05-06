74f9a3dbb2ecefc5ed8a1374d028fa2cd94da254
Karim Siala <karimsiala97@gmail.com>
2024-05-06 14:06:58 +0200

    Update RobotBrowser and BaseCard components

--- frontend/src/components/ui/BaseCard.vue
+++ frontend/src/components/ui/BaseCard.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="overflow-y-auto scroll-auto h-[35rem] bg-gray-900 bg-opacity-40 rounded-lg backdrop-blur-sm shadow-md">
-      <slot></slot>
-    </div>
-  </template>
-  
+  <div class="overflow-y-auto scroll-auto h-[70rem] bg-gray-900 bg-opacity-40 rounded-lg backdrop-blur-sm shadow-md">
+    <slot></slot>
+  </div>
+</template>
