491948e7c9418821ad3d3d2f8ecd13b44f41f8f5
Karim Siala <karimsiala97@gmail.com>
2024-06-22 19:28:42 +0200

    remove version ui box

--- frontend/src/components/layout/InfoPanel.vue
+++ frontend/src/components/layout/InfoPanel.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <div
-            :class="`fixed flex bottom-6 right-10 top-12 h-[8%] w-[6%] bg-white bg-opacity-5 backdrop-blur-sm shadow-md justify-between items-center text-white z-10 rounded-lg  `">
+            :class="`fixed flex bottom-1 right-2 top-1 h-[4%] w-[8  %] justify-between items-center text-white z-10 rounded-lg  `">
 
             <div class="flex space-x-2 md:space-x-8 lg:space-x-12 flex-wrap">
                 <div
-                    class="tracking-[0.1em] font bold flex flex-col justify-center items-center text-center text-white p-4 text-xs md:text-l lg:text-3xl">
+                    class="tracking-[0.1em] font bold flex flex-col justify-center items-center text-center text-white p-4 text-xs md:text-l lg:text-xl">
                     mvp v0.2
                 </div>
 
@@ -15,5 +15,4 @@
 </template>
 
 <script setup lang="ts">
-import BaseCard from '../ui/BaseCard.vue';
 </script>
