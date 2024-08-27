8843399917c623ed8688d6a39f4dacc77bd783da
Karim Siala <karimsiala97@gmail.com>
2024-08-27 10:03:19 +0200

    feat: Add Netlify attribution logo to default layout

--- frontend/src/components/default-layout.tsx
+++ frontend/src/components/default-layout.tsx
@@ -12,6 +12,17 @@ const DefaultLayout = () => {
       <div className="absolute top-2 right-5 text-white"> mvp v0.4</div>
       <MainNav onButtonClick={handleOpenedWindow} />
       <Window position="right" windowTag={openedWindow} />
+      <div className="absolute bottom-4 left-7 text-white">
+        {" "}
+        <a href="https://www.netlify.com">
+          {" "}
+          <img
+            src="/netlify-dark.svg"
+            alt="Deploys by Netlify"
+            className="h-auto w-auto"
+          />{" "}
+        </a>
+      </div>
     </div>
   );
 };
