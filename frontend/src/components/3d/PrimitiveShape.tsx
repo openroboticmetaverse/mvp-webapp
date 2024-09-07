822619c41d48e680f8ae9e303ba1c784cf08ea31
Karim Siala <karimsiala97@gmail.com>
2024-09-07 08:45:18 +0200

    refactor: Optimize PrimitiveShape component for better performance

--- frontend/src/components/3d/PrimitiveShape.tsx
+++ frontend/src/components/3d/PrimitiveShape.tsx
@@ -1,38 +1,50 @@
-import React from "react";
-import { CustomObject3DProps } from "./CustomObject3D";
+import React, { useMemo } from "react";
+import { Sphere, Box, Cylinder, Cone, Torus } from "@react-three/drei";
 import CustomObject3D from "./CustomObject3D";
+import * as THREE from "three";
 
-type PrimitiveShapeProps = Omit<CustomObject3DProps, "children"> & {
+interface PrimitiveShapeProps {
   shape: "sphere" | "cube" | "cylinder" | "cone" | "torus";
   isSelected?: boolean;
-};
+  position: [number, number, number];
+  sceneId?: string;
+  name?: string;
+  color?: string;
+}
 
 const PrimitiveShape: React.FC<PrimitiveShapeProps> = ({
   shape,
-  isSelected,
+  isSelected = false,
+  position,
+  color,
   ...props
 }) => {
+  const material = useMemo(
+    () => new THREE.MeshStandardMaterial({ color }),
+    [color]
+  );
+
   const getGeometry = () => {
+    const sharedProps = { material };
     switch (shape) {
       case "sphere":
-        return <sphereGeometry args={[1, 32, 32]} />;
+        return <Sphere args={[1, 32, 32]} {...sharedProps} />;
       case "cube":
-        return <boxGeometry args={[1, 1, 1]} />;
+        return <Box args={[1, 1, 1]} {...sharedProps} />;
       case "cylinder":
-        return <cylinderGeometry args={[1, 1, 2, 32]} />;
+        return <Cylinder args={[1, 1, 2, 32]} {...sharedProps} />;
       case "cone":
-        return <coneGeometry args={[1, 2, 32]} />;
+        return <Cone args={[1, 2, 32]} {...sharedProps} />;
       case "torus":
-        return <torusGeometry args={[1, 0.4, 16, 100]} />;
+        return <Torus args={[1, 0.4, 16, 100]} {...sharedProps} />;
+      default:
+        return null;
     }
   };
 
   return (
-    <CustomObject3D {...props}>
-      <mesh>
-        {getGeometry()}
-        <meshMatcapMaterial color={isSelected ? "blue" : "cyan"} />
-      </mesh>
+    <CustomObject3D position={position} {...props}>
+      {getGeometry()}
     </CustomObject3D>
   );
 };
