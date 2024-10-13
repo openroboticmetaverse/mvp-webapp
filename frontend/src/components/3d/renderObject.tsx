bb9f5d937169d3b97ffd6ec9b15a496daeb29d5c
Karim Siala <karimsiala97@gmail.com>
2024-10-13 19:21:19 +0200

    Refactor renderObject component to support loading different 3D file types

--- frontend/src/components/3d/renderObject.tsx
+++ frontend/src/components/3d/renderObject.tsx
@@ -1,26 +1,40 @@
-import { IObject } from "@/types/Interfaces";
+import { useLoader } from "@react-three/fiber";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { IObject, IReferenceObject } from "@/types/Interfaces";
 
-/**
- * Renders a 3D object according to the given ObjectData.
- * The object is clickable; when clicked, it sets the selected ID to the ID of the object.
- *
- * @param obj The ObjectData to render.
- * @param setSelectedId The callback to set the selected ID.
- * @returns A JSX element representing the object mesh.
- */
 export const renderObject = (
   obj: IObject,
-  setSelectedId: (id: string | null) => void
+  referenceObject: IReferenceObject
 ) => {
-  return (
-    <mesh
-      onClick={(event) => {
-        event.stopPropagation();
-        setSelectedId(obj.id);
-      }}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshMatcapMaterial color={obj.color} />
-    </mesh>
-  );
+  switch (referenceObject.file_type.toLowerCase()) {
+    case "stl":
+      const geometry = useLoader(
+        STLLoader,
+        URL.createObjectURL(referenceObject.file)
+      );
+      return <primitive object={geometry} />;
+    case "obj":
+      const objModel = useLoader(
+        OBJLoader,
+        URL.createObjectURL(referenceObject.file)
+      );
+      return <primitive object={objModel} />;
+    case "gltf":
+    case "glb":
+      const gltf = useLoader(
+        GLTFLoader,
+        URL.createObjectURL(referenceObject.file)
+      );
+      return <primitive object={gltf.scene} />;
+    default:
+      // Fallback to basic shape
+      return (
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={obj.color} />
+        </mesh>
+      );
+  }
 };
