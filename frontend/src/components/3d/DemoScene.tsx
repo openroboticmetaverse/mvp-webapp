81503ba1f05ff6964f6a6ba019f11a705e61e85d
Karim Siala <karimsiala97@gmail.com>
2024-10-23 09:56:01 +0200

    Refactor DemoScene component to update joint configurations and WebSocket URLs

--- frontend/src/components/3d/DemoScene.tsx
+++ frontend/src/components/3d/DemoScene.tsx
@@ -8,8 +8,18 @@ import RobotTarget from "./RobotTarget";
 
 const DemoScene: React.FC = () => {
   // Optional custom joint configuration
-  const jointConfig = [
-    { name: "Base", axis: "rx" as const },
+  const jointConfig_1 = [
+    { name: "Base", axis: "" as const },
+    { name: "Link1", axis: "rz" as const },
+    { name: "Link2", axis: "ry" as const },
+    { name: "Link3", axis: "ry" as const },
+    { name: "Link4", axis: "rz" as const },
+    { name: "Link5", axis: "ry" as const },
+    { name: "Link6", axis: "rz" as const },
+  ];
+
+  const jointConfig_2 = [
+    { name: "Base", axis: "" as const },
     { name: "Link1", axis: "rz" as const },
     { name: "Link2", axis: "ry" as const },
     { name: "Link3", axis: "ry" as const },
@@ -18,13 +28,25 @@ const DemoScene: React.FC = () => {
     { name: "Link6", axis: "ry" as const },
   ];
 
-  const websocketConfig = {
-    url: "ws://134.61.134.249:8081/",
+  const websocketConfig_1 = {
+    url: "ws://134.61.134.249:8082/",
     reconnectAttempts: 5,
     reconnectInterval: 5000,
     messageType: "joint_states", // Optional: specify if server sends typed messages
   };
-
+  const websocketConfig_2 = {
+    url: "ws://134.61.134.249:8081/",
+    reconnectAttempts: 5,
+    reconnectInterval: 5000,
+    messageType: "joint_states",
+  };
+  /*   const websocketConfig_Local = {
+    url: "ws://localhost:8085",
+    reconnectAttempts: 5,
+    reconnectInterval: 5000,
+    messageType: "joint_states",
+  };
+ */
   return (
     <>
       {/* MCXRobot with required props */}
@@ -40,13 +62,29 @@ const DemoScene: React.FC = () => {
       >
         <SceneEnvironment />
         <MCXRobot
-          modelUrl="/models/MCX-Anthropomorphic2-Robot-R00.glb"
+          modelUrl="/models/MCX-Anthropomorphic-Robot-R01.glb"
           position={[0, 0, 0]}
-          jointConfig={jointConfig}
-          websocketConfig={websocketConfig}
-          id="demo-robot"
+          scale={[2, 2, 2]}
+          jointConfig={jointConfig_1}
+          websocketConfig={websocketConfig_1}
+          id="motorcortex-robot"
         />
-        <RobotTarget websocketUrl={"ws://134.61.134.249:8082/"} />
+        <MCXRobot
+          modelUrl="/models/MCX-Anthropomorphic2-Robot-R00.glb"
+          position={[-2, 0, 0]}
+          scale={[2, 2, 2]}
+          jointConfig={jointConfig_2}
+          websocketConfig={websocketConfig_2}
+          id="mujoco-robot"
+        />
+        {/*         <MCXRobot
+          modelUrl="/models/MCX-Anthropomorphic2-Robot-R00.glb"
+          position={[2, 0, 0]}
+          jointConfig={jointConfig}
+          websocketConfig={websocketConfig_Local}
+          id="local-robot"
+          debug={true}
+        /> */}
       </Canvas>
     </>
   );
